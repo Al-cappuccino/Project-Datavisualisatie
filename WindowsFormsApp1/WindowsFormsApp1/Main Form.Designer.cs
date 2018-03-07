6f14c0357c63357b340a72d978cf779e857f1468
Nathan Drijver (0949442) <0949442@hr.nl>
2018-03-07 12:27:06 +0100

    well fug

--- WindowsFormsApp1/WindowsFormsApp1/Main Form.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Main Form.Designer.cs
@@ -28,19 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(163, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1010, 539);
+            this.dataGridView1.TabIndex = 0;
             // 
             // Main_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(986, 481);
+            this.ClientSize = new System.Drawing.Size(1315, 592);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main_Form";
             this.Text = "Main_Form";
+            this.Load += new System.EventHandler(this.Main_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/Main Form.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Main Form.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Odbc;
 
 namespace WindowsFormsApp1
 {
     public partial class Main_Form : Form
     {
+        DataSet fug;
+        OdbcConnection con;
+        OdbcDataAdapter adapter;
         public Main_Form()
         {
             InitializeComponent();
+        }
+
+        private void Main_Form_Load(object sender, EventArgs e)
+        {
+            fug = new DataSet();
+            con = new OdbcConnection();
+            con.ConnectionString = "Driver={Microsoft Excel Driver (*.xls)};DBQ=Misdrijven fysiek letsel.xls";
+            con.Open();
+            adapter = new OdbcDataAdapter("select * FROM [Misdrijven$]", con);
+            adapter.Fill(fug);
+            con.Close();
+            dataGridView1.DataSource = fug.Tables[0];
         }
     }
 }
