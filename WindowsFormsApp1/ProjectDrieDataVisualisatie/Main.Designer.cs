c9aa11690c37b436b8b2f4b1aebdf2377e12a182
René van Til <ploxdontspamme>
2018-04-02 20:30:48 +0200

    Changed text in home

--- WindowsFormsApp1/ProjectDrieDataVisualisatie/Main.Designer.cs
+++ WindowsFormsApp1/ProjectDrieDataVisualisatie/Main.Designer.cs
@@ -299,7 +299,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(823, 40);
             this.textBox2.TabIndex = 1;
-            this.textBox2.Text = "TDNFR is een programma waarin misdaad en letsel ";
+            this.textBox2.Text = "TDFNR is een programma waarin misdaad en letsel ";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox7
